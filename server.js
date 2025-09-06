0bce9aa4a691bc8c38c2f58da36db9dd5233989f
visrut <visrutnavadiya111@gmail.com>
2025-09-06 15:16:08 +0530

    fix: make server code secure and serve static content from public directory

--- server.js
+++ server.js
@@ -1,41 +1,41 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+import { createServer } from "http";
+import { readFile } from "fs";
+import path from "path";
+import { join, extname } from "path";
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = 3000;
+const publicDir = join(__dirname, "public");
 
-const server = http.createServer((req, res) => {
-  let filePath = "";
+const server = createServer((req, res) => {
+  let filePath = join(publicDir, req.url === "/" ? "index.html" : req.url);
 
-  if (req.url === "/") {
-    filePath = path.join(__dirname, "index.html");
-  } else {
-    // serve other files like CSS
-    filePath = path.join(__dirname, req.url);
-  }
-
-  fs.readFile(filePath, (err, data) => {
+  readFile(filePath, (err, data) => {
     if (err) {
       res.writeHead(404);
       res.end("Not found");
-    } else {
-      // Simple content type handling
-      const ext = path.extname(filePath);
-      let contentType = "text/html";
-      if (ext === ".css") contentType = "text/css";
-      if (ext === ".js") contentType = "text/javascript";
-
-      // Only set cookies for index.html
-      if (req.url === "/") {
-        res.setHeader("Set-Cookie", [
-          "normalCookie=IAmJSAccessible; Path=/",
-          "httpOnlyCookie=IAmHttpOnly; HttpOnly; Path=/",
-        ]);
-      }
-
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(data);
+      return;
     }
+
+    const ext = extname(filePath);
+    let contentType = "text/html";
+    if (ext === ".css") contentType = "text/css";
+    if (ext === ".js") contentType = "text/javascript";
+
+    // Set cookies only for index.html
+    if (req.url === "/") {
+      res.setHeader("Set-Cookie", [
+        "normalCookie=IAmJSAccessible; Path=/",
+        "httpOnlyCookie=IAmHttpOnly; HttpOnly; Path=/",
+      ]);
+    }
+
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(data);
   });
 });
 
